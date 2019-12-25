8f2fb0c48b84fd961b1b9f481268970b602b3402
rdeepak2002 <rdeepak2002@gmail.com>
2019-12-25 12:03:44 -0800

    added improvements

--- static/home.js
+++ static/home.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
 					})
 			.done(function(data) {
 				console.log(data.output);
-				$('#output').text(data.output).show();
+				$('#output').text("You have " + data.output).show();
 					$("#loader").removeClass("loader");
 			});
 		}
